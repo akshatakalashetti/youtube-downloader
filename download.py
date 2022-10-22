8192e0c335381f3c2bad87d32446bead55a91440
akshatakalashetti <akshatakalashetty2001@gmail.com>
2022-10-22 23:25:28 +0530

    changes

--- download.py
+++ download.py
@@ -1,2 +1,17 @@
-youtube_link=input()
-firefox --new-tab "https://en.savefrom.net/1-youtube-video-downloader-384/"
+
+#firefox --new-tab "https://en.savefrom.net/1-youtube-video-downloader-384/"
+
+
+
+
+
+    # Finding the search field by id
+input = driver.find_element_by_id("sf_url")
+     
+    # Sending input text to search field
+input.send_keys("Python")
+     
+    # Pressing enter to search input text
+input.send_keys(Keys.ENTER)
+sleep(10)
+input()
