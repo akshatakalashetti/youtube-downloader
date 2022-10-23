a20866b6c6b4c362eff7a103a29df667677613f7
akshatakalashetti <akshatakalashetty2001@gmail.com>
2022-10-23 20:49:44 +0530

    completing youtube-downloader

--- download.py
+++ download.py
@@ -1,17 +1,22 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from time import sleep
+driver = webdriver.Chrome(ChromeDriverManager().install())
+#opening link 
+driver.get("https://en.savefrom.net/1-youtube-video-downloader-384/")
+driver.close
+#searching search bar with path
+search_bar = driver.find_element_by_xpath('//*[@id="sf_url"]')
+enter_link= input("Enter the youtube link you want to download")
+search_bar.send_keys(enter_link)
+#clicking submit button
+submit_button  = driver.find_element_by_id("sf_submit").click() 
+#waiting  to search
+sleep(5)
 
-#firefox --new-tab "https://en.savefrom.net/1-youtube-video-downloader-384/"
+print("downloading....")
+#clicking download button
+elements = driver.find_element_by_xpath('//*[@id="sf_result"]/div/div[1]/div[2]/div[2]/div[1]/a').click()
+print("video downloaded....")
 
-
-
-
-
-    # Finding the search field by id
-input = driver.find_element_by_id("sf_url")
-     
-    # Sending input text to search field
-input.send_keys("Python")
-     
-    # Pressing enter to search input text
-input.send_keys(Keys.ENTER)
-sleep(10)
-input()
